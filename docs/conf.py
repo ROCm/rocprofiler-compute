db81a8786ebb159e444633bc781e9eb536fc4b56
Karl W. Schulz <karl.schulz@amd.com>
2025-01-14 15:13:23 -0600

    add license delimiter at top of file
    Signed-off-by: Karl W. Schulz <karl.schulz@amd.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+##############################################################################bl
 # MIT License
 
 # Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
@@ -19,6 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+##############################################################################el
 
 # Configuration file for the Sphinx documentation builder.
 #
