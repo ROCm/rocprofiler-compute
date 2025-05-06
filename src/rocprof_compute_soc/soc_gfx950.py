7d149e74d6917076a8a166e0171c182ba2294dc5
Xuan Chen <xuchen@amd.com>
2025-05-06 19:50:31 +0000

    isort.

--- src/rocprof_compute_soc/soc_gfx950.py
+++ src/rocprof_compute_soc/soc_gfx950.py
@@ -27,9 +27,9 @@ from pathlib import Path
 import config
 from rocprof_compute_soc.soc_base import OmniSoC_Base
 from roofline import Roofline
+from utils.logger import console_error, console_log, console_warning, demarcate
 from utils.mi_gpu_spec import mi_gpu_specs
-from utils.logger import demarcate
-from utils.utils import console_error, console_log, console_warning, mibench
+from utils.utils import mibench
 
 
 class gfx950_soc(OmniSoC_Base):
