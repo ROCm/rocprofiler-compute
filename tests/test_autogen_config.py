0eab0de1c8f2c9574c3d0ff8f0022509cee0f5c0
Vignesh Edithal <Vignesh.Edithal@amd.com>
2025-08-05 10:49:01 -0400

    remove unnecessary comment

--- tests/test_autogen_config.py
+++ tests/test_autogen_config.py
@@ -7,11 +7,6 @@ import yaml
 
 @pytest.mark.autogen_config
 def test_modification_time():
-    # Ensure modification time of utils/unified_config.yaml is older than
-    # utils/autogen_hash.yaml
-    # docs/data/metrics_description.yaml and
-    # src/rocprof_compute_soc/analysis_configs/gfx*/*.yaml
-
     # Ensure hash map consistency
     hash_path = Path("utils/autogen_hash.yaml")
     with open(hash_path, "r") as f:
